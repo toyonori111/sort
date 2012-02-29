9419d7c8668cbd84bed40ac6412379f0f5522e2b
noriyuki toyota <ntoyota950@gmail.com>
2012-02-29 19:33:02 +0900

    totyuu

---
+++ bucketsortMain.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+#include "bucketsort.hpp"
+using namespace std;
+
+int main(){
+  char word[50];
+  cout << ">";
+  cin >> word ;
+  
+  bucketsort().sort(word);
+}
